72200b11bd60d4dd76fb4362d10a0a84c68ca83b
neelworking <neelwoking@gmail.com>
2025-03-18 11:46:20 +0530

    add

---
+++ features/counter/counterSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += Number(action.payload);
+    },
+    decrementByAmount: (state, action) => {
+      state.value -= Number(action.payload);
+    },
+    countReset: (state) => {
+      state.value = 0;
+    },
+  },
+});
+
+export const {
+  increment,
+  decrement,
+  incrementByAmount,
+  countReset,
+  decrementByAmount,
+} = counterSlice.actions;
+
+export default counterSlice.reducer;
---
+++ redux/store.js
@@ -0,0 +1,6 @@
+import counterSlice from "@/features/counter/counterSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: { counter: counterSlice },
+});
